3bd5789a79f3a7f0652b39d696568fb95495f020
Kuja Dconnor <rubynapi321@gmail.com>
2021-02-04 21:33:08 +0700

    Add build files

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Product from "../views/Product.vue";
+import Cart from "../views/Cart.vue";
+import GenderOverview from "../views/GenderOverview.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +12,21 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/products/:id",
+    name: "Product",
+    component: Product
+  },
+  {
+    path: "/cart",
+    name: "Cart",
+    component: Cart,
+  },
+  {
+    path: "/:gender/",
+    name: "gender-overview",
+    component: GenderOverview,
   },
   {
     path: "/about",
